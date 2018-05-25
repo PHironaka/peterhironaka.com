389cd6fbebf378a5b5e9f479e0becced3cf43fe7
PHironaka <peterhironaka@mac.com>
2018-05-25 08:46:34 -0700

    added twitter image n meta tags

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -17,10 +17,11 @@ const TemplateWrapper = ({ children }) => (
         		<meta property="og:type" content="website" />
         		<meta name="twitter:site" content="@peterhironaka" />
           <meta name="description" content="Peter Hironaka is a Freelance Web Developer based in Los Angeles." />
-
+          <meta name="twitter:image" value={pistol} />
+          <meta property="og:image" content={pistol} />
         <link rel="icon" type="image/png" sizes="32x32" href={pistol} />
 
-       </Helmet>
+      </Helmet>
 
 
     <Navbar />
