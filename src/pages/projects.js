e700f52ea2993d8e171c97bfc42e2b9277abbbb9
PHironaka <peterhironaka@mac.com>
2018-05-25 12:28:14 -0700

    update styling on projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -19,7 +19,7 @@ export default class ProjectPage extends React.Component {
           <FadeIn>  
               <div
                 className="content-post"
-                style={{ border: '1px solid #eaecee', padding: '2em 2em' }}
+                style={{ border: '1px solid #eaecee' }}
                 key={post.id}
               >
                  <div
